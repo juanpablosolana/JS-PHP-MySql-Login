e09cf5d4544b8cce9fbe37caeeeed2705f77a2bf
Juan Pablo Solana Ortiz <juanpablosolana@gmail.com>
2020-07-15 07:23:59 -0500

    sin comentarios raros en los archivos 1.1

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 var formulario = document.getElementById("formulario");
 formulario.addEventListener("submit", function (e) {
   e.preventDefault();
-  //debugger;
-  //console.log("click");
 
   var datos = new FormData(formulario);
-  //console.log(datos.get("usuario"));
-  //console.log(datos.get("password"));
+
   fetch("login.php", {
     method: "POST",
     body: datos,
@@ -16,23 +13,9 @@ formulario.addEventListener("submit", function (e) {
       console.log(data.status);
 
       if (data.status == "ok") {
-        //var usuario = data.user;
-        // debugger;
         location.href = "main.html";
       } else {
-        //   // alert("Hello\nHow are you?✅");
-        //   //HTMLSourceElement.src;
-        console.log(data.status);
-        //debugger;
         alert("Usuario o contraseña incorrectos");
       }
     });
 });
-
-//console.log(data);
-
-// .then((res) => res.json());
-// .then((data) => {
-//   console.log(data);
-// debugger;
-//  });
